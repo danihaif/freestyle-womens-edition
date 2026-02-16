4aea4777552a6f76add7464c5974e99dd4d3652e
Daniel Gal <danihaif@gmail.com>
2026-02-16 23:25:08 +0200

    FIx deployment

--- src/App.tsx
+++ src/App.tsx
@@ -572,7 +572,7 @@ const App: React.FC = () => {
                   title="YouTube video player"
                   frameBorder="0"
                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                  referrerPolicy="strict-origin-when-cross-compilance"
+                  referrerPolicy="strict-origin-when-cross-origin"
                   allowFullScreen
                 ></iframe>
               </div>
